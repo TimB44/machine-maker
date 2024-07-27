0a51833d36f2f88fc161fd3688c571cb0012472e
TimB44 <tblamires22@gmail.com>
2024-07-26 22:58:52 -0600

    Added more testing for dfa, and switched from usize to 16bit. Clossed issue #1, issue #2

--- engine-room/src/dfa.rs
+++ engine-room/src/dfa.rs
@@ -1,22 +1,23 @@
-use std::collections::HashSet;
-
-#[cfg(test)]
-mod dfa_tests;
+use crate::{
+    machine_utils::{table_lookup, validate_input},
+    StateMachine, TapeMovement,
+};
+use std::{collections::HashSet, u16, usize};
 pub struct Dfa {
-    transition_table: Vec<usize>,
-    accept_states: HashSet<usize>,
-    // max_state: usize,
-    max_char: usize,
+    transition_table: Vec<u16>,
+    accept_states: HashSet<u16>,
+    // max_state: u16,
+    max_char: u16,
 }
 
 impl Dfa {
     pub fn build(
-        transition_table: Vec<usize>,
-        accept_states: HashSet<usize>,
-        max_state: usize,
-        max_char: usize,
+        transition_table: Vec<u16>,
+        accept_states: HashSet<u16>,
+        max_state: u16,
+        max_char: u16,
     ) -> Result<Dfa, ()> {
-        if transition_table.len() != (max_state + 1) * (max_char + 1) {
+        if transition_table.len() != ((max_state + 1) * (max_char + 1)) as usize {
             return Err(());
         }
         if transition_table
@@ -35,8 +36,26 @@ impl Dfa {
         })
     }
 
-    pub fn accepts(&self, input: &Vec<usize>) -> Result<bool, ()> {
-        self.validate_input(input)?;
+    fn states<'a>(&'a self, input: &'a [u16]) -> impl Iterator<Item = u16> + 'a {
+        let mut state = 0;
+        Some(0).into_iter().chain(input.iter().map(move |c| {
+            state = self.next_state(state, *c);
+            state
+        }))
+    }
+
+    fn next_state(&self, cur_state: u16, cur_char: u16) -> u16 {
+        self.transition_table[table_lookup(
+            cur_state as usize,
+            cur_char as usize,
+            self.max_char as usize,
+        )]
+    }
+}
+
+impl StateMachine for Dfa {
+    fn accepts(&self, input: &[u16]) -> Result<bool, ()> {
+        validate_input(input, self.max_char)?;
         Ok(self.accept_states.contains(
             &self
                 .states(&input)
@@ -45,29 +64,196 @@ impl Dfa {
         ))
     }
 
-    pub fn state_trace(&self, input: &Vec<usize>) -> Result<Vec<usize>, ()> {
-        self.validate_input(input)?;
+    fn trace_states(&self, input: &[u16]) -> Result<Vec<(u16, Vec<crate::TapeMovement>)>, ()> {
+        validate_input(input, self.max_char)?;
+        let x = Ok(self
+            .states(input)
+            .map(|s| (s, vec![TapeMovement::Right(None)]))
+            .collect());
+        x
+    }
+}
 
-        Ok(self.states(input).collect())
+#[cfg(test)]
+mod dfa_tests {
+
+    use crate::{dfa::Dfa, machine_utils::add_tape_mov, StateMachine};
+    use std::collections::HashSet;
+
+    #[test]
+    fn wrong_length_errors() {
+        let tf_long = vec![0, 1, 1, 0, 1];
+        let tf_correct = vec![0, 1, 1, 0];
+        let tf_short = vec![0, 1, 1];
+        assert!(Dfa::build(tf_long, HashSet::from([1]), 1, 1).is_err());
+        assert!(Dfa::build(tf_short, HashSet::from([1]), 1, 1).is_err());
+        assert!(Dfa::build(tf_correct, HashSet::from([1]), 1, 1).is_ok());
     }
 
-    fn validate_input(&self, input: &Vec<usize>) -> Result<(), ()> {
-        if input.iter().any(|c| c > &self.max_char) {
-            return Err(());
+    #[test]
+    fn wrong_length_uneven_errors() {
+        let tf_long = vec![0; 11];
+        let tf_short = vec![0; 8];
+        let tf_correct = vec![0; 10];
+        assert!(Dfa::build(tf_long, HashSet::from([1]), 1, 4).is_err());
+        assert!(Dfa::build(tf_short, HashSet::from([1]), 1, 4).is_err());
+        assert!(Dfa::build(tf_correct, HashSet::from([1]), 1, 4).is_ok());
+    }
+
+    #[test]
+    fn invalid_accept_states() {
+        let tf = vec![0; 20];
+        assert!(Dfa::build(tf.clone(), HashSet::from([5]), 4, 3).is_err());
+        assert!(Dfa::build(tf.clone(), HashSet::from([4, 5]), 4, 3).is_err());
+        assert!(Dfa::build(tf.clone(), HashSet::from([500]), 4, 3).is_err());
+        assert!(Dfa::build(tf.clone(), HashSet::from([0, 1, 2, 3, 4]), 4, 3).is_ok());
+    }
+
+    #[test]
+    fn to_large_state_errors() {
+        let tf_correct = vec![0, 1, 2, 2, 1, 1];
+        let tf_one_to_large = vec![0, 1, 2, 3, 1, 1];
+        let tf_way_to_large = vec![10, 734, 234, 12, 93, 1523];
+
+        assert!(Dfa::build(tf_correct, HashSet::from([0]), 2, 1).is_ok());
+        assert!(Dfa::build(tf_one_to_large, HashSet::from([0]), 2, 1).is_err());
+        assert!(Dfa::build(tf_way_to_large, HashSet::from([0]), 2, 1).is_err());
+    }
+
+    #[test]
+    fn invalid_input() {
+        let correct = vec![0, 1, 1, 0];
+        let small = vec![0, 1, 2, 1, 2, 1];
+        let large = vec![5325, 2325, 23564, 3252, 312];
+        let dfa = Dfa::build(vec![1, 0, 1, 0], HashSet::from([0]), 1, 1).unwrap();
+
+        assert!(dfa.accepts(&correct).is_ok());
+        assert!(dfa.accepts(&small).is_err());
+        assert!(dfa.accepts(&large).is_err());
+
+        assert!(dfa.trace_states(&correct).is_ok());
+        assert!(dfa.trace_states(&small).is_err());
+        assert!(dfa.trace_states(&large).is_err());
+    }
+
+    #[test]
+    fn invalid_input_many_chars() {
+        let dfa = Dfa::build(vec![0; 500], HashSet::from([0]), 9, 49).unwrap();
+        let long_valid: Vec<u16> = (0..1_000).into_iter().map(|n| n % 50).collect();
+        let long_invalid_barley: Vec<u16> = (0..1_000).into_iter().map(|n| n % 51).collect();
+        let long_invalid: Vec<u16> = (0..1_000).into_iter().collect();
+
+        assert!(dfa.accepts(&long_valid).is_ok());
+        assert!(dfa.accepts(&long_invalid_barley).is_err());
+        assert!(dfa.accepts(&long_invalid).is_err());
+
+        assert!(dfa.trace_states(&long_valid).is_ok());
+        assert!(dfa.trace_states(&long_invalid_barley).is_err());
+        assert!(dfa.trace_states(&long_invalid).is_err());
+    }
+
+    #[test]
+    fn valid_input_empty() {
+        let dfa = Dfa::build(vec![1, 0, 1, 0], HashSet::from([0]), 1, 1).unwrap();
+        let input = vec![];
+        assert!(dfa.trace_states(&input).is_ok());
+        assert!(dfa.accepts(&input).is_ok());
+    }
+
+    #[test]
+    fn accepts_odd_length_dfa() {
+        let odd_len_dfa = Dfa::build(vec![1, 0], HashSet::from([1]), 1, 0).unwrap();
+        let expected_states = add_tape_mov(vec![0, 1].repeat(100));
+        for len in 3..100 {
+            let input = vec![0].repeat(len);
+            assert_eq!(odd_len_dfa.accepts(&input).unwrap(), len % 2 == 1);
+            assert_eq!(
+                odd_len_dfa.trace_states(&input).unwrap(),
+                expected_states[..(len + 1)]
+            )
         }
-        Ok(())
     }
 
-    fn states<'a>(&'a self, input: &'a Vec<usize>) -> impl Iterator<Item = usize> + 'a {
-        let mut state = 0;
-        Some(0).into_iter().chain(input.iter().map(move |c| {
-            state = self.next_state(state, *c);
-            state
-        }))
+    #[test]
+    fn accepts_end_in_2() {
+        let tf = vec![0, 0, 1, 0, 0, 1];
+        let end_in_2_dfa = Dfa::build(tf, HashSet::from([1]), 1, 2).unwrap();
+        assert!(!end_in_2_dfa.accepts(&vec![0, 1, 0, 1, 0, 1, 0]).unwrap());
+        assert_eq!(
+            end_in_2_dfa
+                .trace_states(&vec![0, 1, 0, 1, 0, 1, 0])
+                .unwrap(),
+            add_tape_mov(vec![0, 0, 0, 0, 0, 0, 0, 0])
+        );
+
+        assert!(!end_in_2_dfa
+            .accepts(&vec![0, 1, 0, 1, 0, 2, 2, 0, 1, 2, 1])
+            .unwrap());
+        assert_eq!(
+            end_in_2_dfa
+                .trace_states(&vec![0, 1, 0, 1, 0, 2, 2, 0, 1, 2, 1])
+                .unwrap(),
+            add_tape_mov(vec![0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0])
+        );
+
+        assert!(!end_in_2_dfa.accepts(&vec![0, 1, 2, 1]).unwrap());
+        assert_eq!(
+            end_in_2_dfa.trace_states(&vec![0, 1, 2, 1]).unwrap(),
+            add_tape_mov(vec![0, 0, 0, 1, 0])
+        );
+
+        assert!(end_in_2_dfa.accepts(&vec![0, 0, 0, 2]).unwrap());
+        assert_eq!(
+            end_in_2_dfa.trace_states(&vec![0, 0, 0, 2]).unwrap(),
+            add_tape_mov(vec![0, 0, 0, 0, 1])
+        );
+
+        assert!(end_in_2_dfa.accepts(&vec![0, 1, 2]).unwrap());
+        assert_eq!(
+            end_in_2_dfa.trace_states(&vec![0, 1, 2]).unwrap(),
+            add_tape_mov(vec![0, 0, 0, 1])
+        );
+
+        assert!(end_in_2_dfa.accepts(&vec![2, 2, 2, 2, 2]).unwrap());
+        assert_eq!(
+            end_in_2_dfa.trace_states(&vec![2, 2, 2, 2, 2]).unwrap(),
+            add_tape_mov(vec![0, 1, 1, 1, 1, 1])
+        );
+
+        assert!(end_in_2_dfa.accepts(&vec![2, 2, 0, 2]).unwrap());
+        assert_eq!(
+            end_in_2_dfa.trace_states(&vec![2, 2, 0, 2]).unwrap(),
+            add_tape_mov(vec![0, 1, 1, 0, 1])
+        );
     }
 
-    // TODO convert to vec<u8> and use max_state to determine # of bytes
-    fn next_state(&self, cur_state: usize, char: usize) -> usize {
-        self.transition_table[cur_state * (self.max_char + 1) + char]
+    #[test]
+    fn accepts_100_len() {
+        let mut tf = vec![];
+        for next in 1..=101 {
+            tf.extend_from_slice(&vec![next].repeat(10));
+        }
+        tf.extend_from_slice(&vec![101].repeat(10));
+        let accepts_len_100 = Dfa::build(tf, HashSet::from([100]), 101, 9).unwrap();
+        assert!(accepts_len_100
+            .accepts(&vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9].repeat(10))
+            .unwrap());
+        assert_eq!(
+            accepts_len_100
+                .trace_states(&vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9].repeat(10))
+                .unwrap(),
+            add_tape_mov((0..=100).collect::<Vec<u16>>())
+        );
+        let mut input = vec![];
+        for i in 0..100 {
+            assert!(!accepts_len_100.accepts(&input).unwrap());
+            input.push(i % 7);
+        }
+        input.push(0);
+
+        for i in 0..100 {
+            assert!(!accepts_len_100.accepts(&input).unwrap());
+            input.push(i % 7);
+        }
     }
 }
--- engine-room/src/lib.rs
+++ engine-room/src/lib.rs
@@ -1,2 +1,59 @@
 pub mod dfa;
+mod machine_utils;
+pub mod multi_tm;
 pub mod nfa;
+pub mod pda;
+pub mod stay_tm;
+pub mod tm;
+
+/// # State Machine
+///
+/// This trait defines the essential methods for a state machine. A state machine
+/// can determine if it accepts a given input and can also trace the states it
+/// passes through during the input processing.
+///
+/// For DFA and NFA, the `state_trace` method will always return movements
+/// of `Right(None)` as these state machines only read the tape once without
+/// modifying it.
+///
+pub trait StateMachine {
+    /// Checks if the state machine accepts the given input.
+    fn accepts(&self, input: &[u16]) -> Result<bool, ()>;
+
+    /// Traces the states and movements of the state machine for the given input.
+    fn trace_states(&self, input: &[u16]) -> Result<Vec<(u16, Vec<TapeMovement>)>, ()>;
+}
+
+/// # Tape Movement
+///
+/// This enum defines the possible movements of a tape in a general state machine.
+/// The tape can move left, right, or stay in place. Additionally, with each
+/// movement, an optional symbol can be written to the tape. If no symbol is
+/// provided, the tape position will be updated without writing a symbol.
+///
+/// For DFA and NFA, the movement will always be `Right(None)` as these
+/// state machines only interact with the tape by reading it once.
+///
+/// # Examples
+///
+/// ```
+/// use engine_room::TapeMovement;
+///
+/// // Move the tape to the right and write the symbol 1
+/// let move_right = TapeMovement::Right(Some(1));
+///
+/// // Move the tape to the left without writing any symbol
+/// let move_left = TapeMovement::Left(None);
+///
+/// // Keep the tape in the current position and write the symbol 0
+/// let stay_put = TapeMovement::Stay(Some(0));
+///
+/// // Move the tape to the right without writing any symbol
+/// let move_right_no_write = TapeMovement::Right(None);
+/// ```
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum TapeMovement {
+    Right(Option<u16>),
+    Left(Option<u16>),
+    Stay(Option<u16>),
+}
